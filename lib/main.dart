152b896f62747ccb3a6dd398c0f9088d79018b16
CheaVireaksat <cheavireaksat@gmail.com>
2020-06-25 15:01:52 +0700

    adding navigation to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:Shop_App/widgets/product_items.dart';
 import 'package:flutter/material.dart';
 
 import './screens/products_overview_screen.dart';
+import './screens/products_detail_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,6 +20,9 @@ class MyApp extends StatelessWidget {
         fontFamily: 'YanoneKaffeesatz',
       ),
       home: ProductOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+      },
     );
 }
 }
