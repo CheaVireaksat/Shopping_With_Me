b642f3165ef35d3d628c7467a3d75b97f3e0d912
CheaVireaksat <cheavireaksat@gmail.com>
2020-06-26 16:10:17 +0700

    working with provider & listener

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:Shop_App/widgets/product_items.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/products_detail_screen.dart';
+import './providers/products_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,17 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        accentColor: Colors.tealAccent,
-        fontFamily: 'YanoneKaffeesatz',
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          accentColor: Colors.tealAccent,
+          fontFamily: 'YanoneKaffeesatz',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
-}
+  }
 }
