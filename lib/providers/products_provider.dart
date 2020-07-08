719cc7c6e85387d0e21339aa01b8ebc25a965f8a
CheaVireaksat <cheavireaksat@gmail.com>
2020-07-09 00:05:49 +0700

    Updating data via path request

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -110,13 +110,26 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
-    if (prodIndex >= 0) {
-      _items[prodIndex] = newProduct;
-      notifyListeners();
-    } else {
-      print('...');
+    try {
+      if (prodIndex >= 0) {
+        final url = 'https://shopapp-c46c7.firebaseio.com/products/$id.json';
+        await http.patch(url,
+            body: json.encode({
+              'title': newProduct.title,
+              'description': newProduct.description,
+              'imageUrl': newProduct.imageUrl,
+              'price': newProduct.price,
+            }));
+        _items[prodIndex] = newProduct;
+        notifyListeners();
+      } else {
+        print('...');
+      }
+    } catch (error) {
+      print(error);
+      throw error;
     }
   }
 
